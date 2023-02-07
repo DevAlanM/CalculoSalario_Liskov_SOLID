1b398ee5ad5019f43617e541f656eb5c088506fb
DevAlanM <alanmartins789@gmail.com>
2023-02-07 01:58:59 -0300

    realizados pequenos ajustes Liskov

--- src/CalculadoraFilhaViola.java
+++ src/CalculadoraFilhaViola.java
@@ -1,21 +1,27 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class CalculadoraFilhaViola extends CalculadoraPai {
     @Override
     public Double calcular(Double salarioBruto, Double descontos, Double bonus) throws CalculadoraException {
         try {
-            System.out.println("Voce ganhou comissao de 700 reais, digite S para acrecemtar");
+            System.out.println("Voce faltou alguns dias será descontando 700 reais do seu salário, concorda? S/N");
 
             Scanner input = new Scanner(System.in);
             String resposta = input.next();
+            double diminuirSalario = 700.00;
+            if (resposta.equalsIgnoreCase("S")) {
+                salarioBruto -= diminuirSalario;
 
-            Double salarioBrutoAdicional = 0.0;
-            if (resposta == "S") {
-                double decimoterceiro = 700.00;
-                salarioBrutoAdicional = salarioBruto + decimoterceiro;
-                throw new IllegalArgumentException("Argumento invalido");
-            }
-            return super.calcular(salarioBrutoAdicional, descontos, bonus);
+                return super.calcular(salarioBruto, descontos, bonus);
+                //throw new IllegalArgumentException("Argumento invalido");
+            } else if (resposta.equalsIgnoreCase("N")) {
+                System.out.println("Como não aceitou será forçado a pagar, e descontado o dobro do valor");
+                descontos = diminuirSalario * 2;
+
+                return super.calcular(salarioBruto, descontos, bonus);
+            } else
+                return super.calcular(salarioBruto, descontos, bonus);
 
         } catch (Exception e) {
             System.err.println("Errou ai amigão");
